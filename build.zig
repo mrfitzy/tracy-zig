c4e9bab122169bee8fb66407e3d6e8fdca80fb6d
mrfitzy <fitzz86@gmail.com>
2026-08-26 11:56:12 -0700

    add build option for tracy repo path

--- build.zig
+++ build.zig
@@ -1,46 +1,60 @@
 const std = @import("std");
+const LazyPath = std.Build.LazyPath;
 
 pub fn build(b: *std.Build) void {
+    // build options
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+    const tracy_path = b.option([]const u8, "tracy", "Path to tracy repo (enables profiling)");
 
+    // client library
     const lib_client = b.addLibrary(.{
         .name = "tracyclient",
         .linkage = .static,
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
             .target = target,
             .optimize = optimize,
+            .link_libcpp = true,
         }),
     });
+
+    // tracy sources
+    const tracy_public_path: LazyPath = .{
+        .cwd_relative = b.pathJoin(&.{ tracy_path orelse @panic("tracy"), "public" }),
+    };
+    const cpp_flags = &[_][]const u8{
+        "-std=c++23",
+        "-Wall",
+        "-Wformat",
+        "-DTRACY_ENABLE",
+    };
+    lib_client.root_module.addCSourceFiles(.{
+        .root = tracy_public_path,
+        .files = &.{
+            "TracyClient.cpp",
+        },
+        .flags = cpp_flags,
+    });
+    lib_client.root_module.addIncludePath(tracy_public_path);
+
+    // local sources
     lib_client.root_module.addCSourceFiles(.{
         .files = &.{
             "src/stub.cpp",
-            "tracy/public/TracyClient.cpp",
         },
-        .flags = &.{
-            "-std=c++23",
-            "-Wall",
-            "-Wformat",
-            "-DTRACY_ENABLE",
-        },
+        .flags = cpp_flags,
     });
-    lib_client.root_module.link_libcpp = true;
-    lib_client.root_module.addIncludePath(b.path("tracy/public"));
 
-    lib_client.installHeadersDirectory(b.path("tracy/public/client"), "client", .{ .include_extensions = &.{ ".h", ".hpp" } });
-    lib_client.installHeadersDirectory(b.path("tracy/public/common"), "common", .{ .include_extensions = &.{ ".h", ".hpp" } });
-    lib_client.installHeadersDirectory(b.path("tracy/public/tracy"), "tracy", .{ .include_extensions = &.{ ".h", ".hpp" } });
+    // install headers
+    const header_dirs = &[_][]const u8{
+        "client",
+        "common",
+        "tracy",
+    };
+    for (header_dirs) |dir| {
+        lib_client.installHeadersDirectory(tracy_public_path.path(b, dir), dir, .{ .include_extensions = &.{ ".h", ".hpp" } });
+    }
 
+    // install library
     b.installArtifact(lib_client);
-
-    // By making the run step depend on the default step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    //run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
-    //if (b.args) |args| {
-    //    run_cmd.addArgs(args);
-    //}
 }
